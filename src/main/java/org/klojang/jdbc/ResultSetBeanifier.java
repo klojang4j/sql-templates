7912114a2486c4ab6dfd4d92bddb00b5d1d24d34
Ayco Holleman <ayco.holleman@naturalis.nl>
2023-07-05 08:52:33 +0200

    javadocs

--- src/main/java/org/klojang/jdbc/ResultSetBeanifier.java
+++ src/main/java/org/klojang/jdbc/ResultSetBeanifier.java
@@ -5,17 +5,21 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Converts the rows in a JDBC {@link ResultSet result sets} into JavaBeans. Contrary to
- * the {@link SQLQuery} class a {@code ResultSetBeanifier} is completely agnostic about
- * how you got hold of the {@code ResultSet} and by what means it was created. You cannot
- * instantiate a {@code ResultSetBeanifier} directly. You obtain one from a
- * {@link BeanifierFactory}. When using a {@code ResultSetBeanifier} to iterate over a
- * {@code ResultSet}, do not call {@link ResultSet#next()}) yourself. This is done by the
+ * <p>Converts the rows in a JDBC {@link ResultSet} into JavaBeans. Instances are
+ * obtained via
+ * {@link BeanifierFactory#getBeanifier(ResultSet) BeanifierFactory.getBeanifier()}. When
+ * using a {@code ResultSetBeanifier} to iterate over a {@code ResultSet}, do not call
+ * {@link ResultSet#next()}) yourself. This is already done by the
  * {@code ResultSetBeanifier}. Just keep calling {@code #beanify()} until an empty
  * {@code Optional} or {@code List} is returned, or {@link #isEmpty()} returns
  * {@code true}.
  *
- * @param <T> The type of the JavaBeans produced by the {@code ResultSetBeanifier}
+ * <p>Contrary to the {@link SQLQuery} class, which serves a similar purpose, a
+ * {@code ResultSetBeanifier} is agnostic about, and has no opinion on where you got the
+ * {@link ResultSet} from. In that sense it is closer to the JDBC metal than the
+ * {@code SQLQuery} class.
+ *
+ * @param <T> the type of the JavaBeans produced by the {@code ResultSetBeanifier}
  * @author Ayco Holleman
  */
 public sealed interface ResultSetBeanifier<T> extends Iterable<T>
@@ -26,7 +30,7 @@ public sealed interface ResultSetBeanifier<T> extends Iterable<T>
    * the {@code ResultSet} is empty, or if there are no more rows in the
    * {@code ResultSet}, an empty {@code Optional} is returned.
    *
-   * @return An {@code Optional} containing the JavaBean or an empty {@code Optional} if
+   * @return an {@code Optional} containing the JavaBean or an empty {@code Optional} if
    *     the {@code ResultSet} contained no (more) rows
    */
   Optional<T> beanify();
@@ -35,8 +39,8 @@ public sealed interface ResultSetBeanifier<T> extends Iterable<T>
    * Converts at most {@code limit} rows from the specified {@code ResultSet} into
    * JavaBeans. If the {@code ResultSet} is empty, an empty {@code List} is returned.
    *
-   * @param limit maximum number of rows to extract and convert
-   * @return A {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * @param limit the maximum number of rows to extract and convert
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
    *     contained no (more) rows
    */
   List<T> beanify(int limit);
@@ -44,7 +48,7 @@ public sealed interface ResultSetBeanifier<T> extends Iterable<T>
   /**
    * Converts all remaining rows in the specified {@code ResultSet} into JavaBeans.
    *
-   * @return A {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
    *     contained no (more) rows
    */
   List<T> beanifyAll();
@@ -52,8 +56,8 @@ public sealed interface ResultSetBeanifier<T> extends Iterable<T>
   /**
    * Converts all remaining rows n the specified {@code ResultSet} into JavaBeans.
    *
-   * @param sizeEstimate An estimate of the size of the resulting {@code List}.
-   * @return A {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * @param sizeEstimate an estimate of the size of the resulting {@code List}.
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
    *     contained no (more) rows
    */
   List<T> beanifyAll(int sizeEstimate);
