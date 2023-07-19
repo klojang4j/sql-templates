d2060a9c856b67ee29a4e9deebeb418c399b922f
Ayco Holleman <ayco.holleman@naturalis.nl>
2023-07-19 14:44:53 +0200

    javadocs

--- src/main/java/org/klojang/jdbc/Transformer.java
+++ src/main/java/org/klojang/jdbc/Transformer.java
@@ -18,10 +18,10 @@ public interface Transformer<T> {
    * @param bean the bean containing the value
    * @param value the value to be transformed
    * @param quoter a {@link Quoter} instance that you can use if you intend to construct
-   * and return a {@linkplain SQL#expression(String) SQL expression}. <b>Only use a
-   * {@code Quoter} to escape and quote strings <i>within</i> the SQL expression</b>. If
-   * the {@code transform()} method returns a regular {@code String}, rather than a SQL
-   * expression, do not use the {@code Quoter} to escape and quote it.
+   * and return a {@link SQLExpression}. If you return anything else, don't use the
+   * quoter. Escaping and quoting will be taken care of by <i>Klojang JDBC</i> or simply
+   * by JDBC itself. Only use the {@code Quoter} to escape and quote strings <i>within</i>
+   * an SQL expression.
    * @return the value to be saved to the database
    * @throws Throwable if an error occurs while transforming the value
    */
