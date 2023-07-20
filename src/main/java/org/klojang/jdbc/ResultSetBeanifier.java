2a94dcab8bc6b1f1b8aa05851ace4a3710e6b9f4
Ayco Holleman <ayco.holleman@naturalis.nl>
2023-07-20 17:56:30 +0200

    javadocs

--- src/main/java/org/klojang/jdbc/ResultSetBeanifier.java
+++ src/main/java/org/klojang/jdbc/ResultSetBeanifier.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * <p>Converts the rows in a JDBC {@link ResultSet} into JavaBeans. Instances are
- * obtained via
+ * <p>Converts the rows in a JDBC {@link ResultSet} into JavaBeans or records. Instances
+ * are obtained via
  * {@link BeanifierFactory#getBeanifier(ResultSet) BeanifierFactory.getBeanifier()}.
  *
  * <p>Contrary to the {@link SQLQuery} class, a
@@ -28,6 +28,16 @@ import java.util.Optional;
  * }
  * }</pre></blockquote>
  *
+ * <h2>JavaBeans vs. Records</h2>
+ * When converting a row to a JavaBean, a {@code ResultSetBeanifier} will always use the
+ * setters on the JavaBean to populate it. There is no way to populate the bean via its
+ * constructors. When converting to a record, a {@code ResultSetBeanifier} will obviously
+ * always one of its constructors to populate the record (since records don't have
+ * setters). Make sure the record has a constructor for those record components that map
+ * to columns in the SELECT clause.
+ *
+ * <p>A {@code ResultSetBeanifier}
+ *
  * @param <T> the type of the JavaBeans produced by the {@code ResultSetBeanifier}
  * @author Ayco Holleman
  * @see BeanifierFactory
@@ -36,50 +46,50 @@ import java.util.Optional;
 public sealed interface ResultSetBeanifier<T> extends Iterable<T>
       permits DefaultBeanifier, EmptyBeanifier, RecordBeanifier {
 
-    /**
-     * Converts the current row within the {@code ResultSet} into a JavaBean. If the
-     * {@code ResultSet} is empty, or if there are no more rows in the {@code ResultSet},
-     * an empty {@code Optional} is returned. You can keep calling {@code beanify()} to
-     * successively extract all rows in the result set.
-     *
-     * @return an {@code Optional} containing the JavaBean or an empty {@code Optional} if
-     * the {@code ResultSet} contained no (more) rows
-     */
-    Optional<T> beanify();
+  /**
+   * Converts the current row within the {@code ResultSet} into a JavaBean. If the
+   * {@code ResultSet} is empty, or if there are no more rows in the {@code ResultSet}, an
+   * empty {@code Optional} is returned. You can keep calling {@code beanify()} to
+   * successively extract all rows in the result set.
+   *
+   * @return an {@code Optional} containing the JavaBean or an empty {@code Optional} if
+   * the {@code ResultSet} contained no (more) rows
+   */
+  Optional<T> beanify();
 
-    /**
-     * Converts at most {@code limit} rows from the {@code ResultSet} into JavaBeans. If
-     * the {@code ResultSet} is empty, an empty {@code List} is returned.
-     *
-     * @param limit the maximum number of rows to extract and convert
-     * @return a {@code List} of JavaBeans or an empty {@code List} if the
-     * {@code ResultSet} contained no (more) rows
-     */
-    List<T> beanify(int limit);
+  /**
+   * Converts at most {@code limit} rows from the {@code ResultSet} into JavaBeans. If the
+   * {@code ResultSet} is empty, an empty {@code List} is returned.
+   *
+   * @param limit the maximum number of rows to extract and convert
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * contained no (more) rows
+   */
+  List<T> beanify(int limit);
 
-    /**
-     * Converts all remaining rows in the {@code ResultSet} into JavaBeans.
-     *
-     * @return a {@code List} of JavaBeans or an empty {@code List} if the
-     * {@code ResultSet} contained no (more) rows
-     */
-    List<T> beanifyAll();
+  /**
+   * Converts all remaining rows in the {@code ResultSet} into JavaBeans.
+   *
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * contained no (more) rows
+   */
+  List<T> beanifyAll();
 
-    /**
-     * Converts all remaining rows in the {@code ResultSet} into JavaBeans.
-     *
-     * @param sizeEstimate an estimate of the size of the resulting {@code List}.
-     * @return a {@code List} of JavaBeans or an empty {@code List} if the
-     * {@code ResultSet} contained no (more) rows
-     */
-    List<T> beanifyAll(int sizeEstimate);
+  /**
+   * Converts all remaining rows in the {@code ResultSet} into JavaBeans.
+   *
+   * @param sizeEstimate an estimate of the size of the resulting {@code List}.
+   * @return a {@code List} of JavaBeans or an empty {@code List} if the {@code ResultSet}
+   * contained no (more) rows
+   */
+  List<T> beanifyAll(int sizeEstimate);
 
-    /**
-     * Returns {@code true} if the end of the {@code ResultSet} has been reached;
-     * {@code false} otherwise.
-     *
-     * @return whether the end of the {@code ResultSet} has been reached
-     */
-    boolean isEmpty();
+  /**
+   * Returns {@code true} if the end of the {@code ResultSet} has been reached;
+   * {@code false} otherwise.
+   *
+   * @return whether the end of the {@code ResultSet} has been reached
+   */
+  boolean isEmpty();
 
 }
